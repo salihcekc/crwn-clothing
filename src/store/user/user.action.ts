ad4eebc23e4393ceef7587b7a442ad10bb649b04
Salih Çekiç <salihcekc@gmail.com>
2022-12-21 16:07:28 +0300

    typing sagas

--- src/store/user/user.action.ts
+++ src/store/user/user.action.ts
@@ -32,7 +32,7 @@ export type SignInFailed = ActionWithPayload<
 
 export type SignUpStart = ActionWithPayload<
   USER_ACTION_TYPES.SIGN_UP_START,
-  { email: string }
+  { email: string; password: string; displayName: string }
 >;
 
 export type SignUpSuccess = ActionWithPayload<
@@ -65,7 +65,7 @@ export const emailSignInStart = withMatcher(
 );
 
 export const signInSuccess = withMatcher(
-  (user: UserData): SignInSuccess =>
+  (user: UserData & { id: string }): SignInSuccess =>
     createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user)
 );
 
