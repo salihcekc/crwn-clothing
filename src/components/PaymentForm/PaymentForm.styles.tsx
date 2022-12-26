310e8589679a0e38d3ae8caa047aaac5fbe537c7
Salih Çekiç <salihcekc@gmail.com>
2022-12-26 17:26:27 +0300

    styles responsiveness is added for mobile.

--- src/components/PaymentForm/PaymentForm.styles.tsx
+++ src/components/PaymentForm/PaymentForm.styles.tsx
@@ -8,11 +8,26 @@ export const PaymentFormContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  @media screen and (max-width: 800px) {
+    width: 100%;
+    height: 200px;
+    padding-left: 15px;
+    padding-right: 15px;
+    margin-top: 20px;
+  }
 `;
 
 export const FormContainer = styled.form`
-  height: 100px;
+  height: 200px;
   min-width: 500px;
+  padding: 5px;
+
+  @media screen and (max-width: 800px) {
+    min-width: 300px;
+    height: 150px;
+    width: 100%;
+  }
 `;
 
 export const PaymentButton = styled(Button)`
